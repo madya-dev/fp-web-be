c2f9228c5d34293c762c90c81ca2ee9514233017
tudemaha <gede_mahardika88@yahoo.co.id>
2023-06-17 09:46:39 +0800

    udpate cors

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,14 +1,28 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"hrd-be/routes"
 	"log"
+	"time"
 )
 
 func StartServer() {
 	log.Print("INFO StartServer: server is starting")
 	router := gin.Default()
+
+	config := cors.Config{
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+		ExposeHeaders:    []string{"Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	router.Use(cors.New(config))
+
 	router.MaxMultipartMemory = 2 << 20
 	router.Static("files", "./files")
 
