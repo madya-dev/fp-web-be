7a8759a58a1128326e220ec8de46e94f257782ad
tudemaha <gede_mahardika88@yahoo.co.id>
2023-06-19 18:25:01 +0800

    change validation

--- internal/account/dto/request.go
+++ internal/account/dto/request.go
@@ -11,7 +11,7 @@ type CreateInput struct {
 	Role     int     `json:"role" validate:"required"`
 	Name     string  `json:"name" validate:"required"`
 	Age      int     `json:"age" validate:"required"`
-	Salary   float64 `json:"salary" validate:"required"`
+	Salary   float64 `json:"salary" validate:"gte=0"`
 	Position string  `json:"position" validate:"required"`
 	Status   int     `json:"status" validate:"required"`
 }
