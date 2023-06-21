b307b8f8c463de11a481f06cde98e30870ee906a
tudemaha <gede_mahardika88@yahoo.co.id>
2023-06-21 22:57:43 +0800

    change generate slip req body

--- internal/slip/controller/slip_controller.go
+++ internal/slip/controller/slip_controller.go
@@ -31,39 +31,51 @@ func GenerateSlipHandler() gin.HandlerFunc {
 
 		db := database.Connection()
 
-		var employee model.Employee
-		db.Preload("EmployeeStatus").Where("id = ?", generateInput.EmployeeID).
-			Find(&employee)
+		var account model.Account
+		db.Preload("Employee.EmployeeStatus").Where("username = ?", generateInput.Username).
+			Find(&account)
 
 		var cuti, izin, sakit sql.NullInt64
-		db.Raw("SELECT SUM(cis_details.end_date - cis_details.start_date) FROM cis_details INNER JOIN cis ON cis_details.id = cis.cis_detail_id WHERE cis.employee_id = ? AND cis.cis_type_id = ? AND cis_details.start_date >= ? AND cis_details.end_date <= ? AND cis.cis_status_id = 2", generateInput.EmployeeID, 1, generateInput.StartPeriode, generateInput.EndPeriode).
+		stmt := "SELECT SUM(cis_details.end_date - cis_details.start_date) FROM cis_details INNER JOIN cis ON cis_details.id = cis.cis_detail_id WHERE cis.employee_id = ? AND cis.cis_type_id = ? AND cis_details.start_date >= ? AND cis_details.end_date <= ? AND cis.cis_status_id = 3"
+		db.Raw(stmt,
+			account.EmployeeID, 1, generateInput.StartPeriode, generateInput.EndPeriode).
 			Scan(&cuti)
-		db.Raw("SELECT SUM(cis_details.end_date - cis_details.start_date) FROM cis_details INNER JOIN cis ON cis_details.id = cis.cis_detail_id WHERE cis.employee_id = ? AND cis.cis_type_id = ? AND cis_details.start_date >= ? AND cis_details.end_date <= ? AND cis.cis_status_id = 2", generateInput.EmployeeID, 2, generateInput.StartPeriode, generateInput.EndPeriode).
+		db.Raw(stmt,
+			account.EmployeeID, 2, generateInput.StartPeriode, generateInput.EndPeriode).
 			Scan(&izin)
-		db.Raw("SELECT SUM(cis_details.end_date - cis_details.start_date) FROM cis_details INNER JOIN cis ON cis_details.id = cis.cis_detail_id WHERE cis.employee_id = ? AND cis.cis_type_id = ? AND cis_details.start_date >= ? AND cis_details.end_date <= ? AND cis.cis_status_id = 2", generateInput.EmployeeID, 3, generateInput.StartPeriode, generateInput.EndPeriode).
+		db.Raw(stmt,
+			account.EmployeeID, 3, generateInput.StartPeriode, generateInput.EndPeriode).
 			Scan(&sakit)
 
 		var paidLeave, permission, insurance float64
-		var salaryCuts model.SalaryCut
-		db.Where("name = ?", "paid_leave").Find(&salaryCuts)
-		paidLeave = float64(cuti.Int64) * salaryCuts.SalaryCut
+		var salaryCuts []model.SalaryCut
+		db.Find(&salaryCuts)
 
-		db.Where("name = ?", "permission").Find(&salaryCuts)
-		permission = float64(izin.Int64) * salaryCuts.SalaryCut
-
-		db.Where("name = ?", "insurance").Find(&salaryCuts)
-		insurance = salaryCuts.SalaryCut
+		paidLeave = 0
+		permission = 0
+		insurance = 0
+		for _, salaryCut := range salaryCuts {
+			if salaryCut.Name == "paid_leave" {
+				paidLeave = salaryCut.SalaryCut
+			}
+			if salaryCut.Name == "permission" {
+				permission = salaryCut.SalaryCut
+			}
+			if salaryCut.Name == "insurance" {
+				insurance = salaryCut.SalaryCut
+			}
+		}
 
 		startPeriode, _ := time.Parse("2006-01-02", generateInput.StartPeriode)
 		endPeriode, _ := time.Parse("2006-01-02", generateInput.EndPeriode)
 
 		var slip model.SalarySlip
-		slip.Name = employee.Name
-		slip.Position = employee.Position
-		slip.Status = employee.EmployeeStatus.Name
+		slip.Name = account.Employee.Name
+		slip.Position = account.Employee.Position
+		slip.Status = account.Employee.EmployeeStatus.Name
 		slip.StartPeriode = startPeriode
 		slip.EndPeriode = endPeriode
-		slip.BasicSalary = employee.Salary
+		slip.BasicSalary = account.Employee.Salary
 		slip.Bonus = generateInput.Bonus
 		slip.PaidLeave = paidLeave
 		slip.Permission = permission
--- internal/slip/dto/request.go
+++ internal/slip/dto/request.go
@@ -1,8 +1,8 @@
 package dto
 
 type GenerateInput struct {
-	EmployeeID   int     `json:"employee_id" validate:"required"`
-	Bonus        float64 `json:"bonus" validate:"required"`
+	Username     string  `json:"username" validate:"required"`
+	Bonus        float64 `json:"bonus" validate:"gte=0"`
 	StartPeriode string  `json:"start_periode" validate:"required,min=10,max=10"`
 	EndPeriode   string  `json:"end_periode" validate:"required,min=10,max=10"`
 }
